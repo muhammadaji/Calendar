e0cb61bf0ad1f5b5e9d2c9351c154998e7aa09ee
Muhammad Aji Raharjo <loroyangtau@gmail.com>
2019-02-23 00:43:03 +0700

    Styling a bit

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, StyleSheet}  from 'react-native';
-import { Container, Icon, Button, Form, Item, Input, Text, DatePicker} from 'native-base';
+import { Container, Icon, Button, Form, Item, Input, DatePicker, Text} from 'native-base';
 import ActionButton from 'react-native-action-button';
 import { Calendar } from 'react-native-calendars';
 import Modal from 'react-native-modal';
@@ -32,7 +32,16 @@ export default class App extends Component {
       date: this.state.choosenDate,
       selected: true,
       eventName: this.state.eventName,
-      eventDesc: this.state.eventDesc
+      eventDesc: this.state.eventDesc,
+      customStyles: {
+        container: {
+          backgroundColor: '#209fd5',
+          elevation: 7
+        },
+        text: {
+          color: 'white'
+        }
+      }
     }
     this.state.markedDates.push(setDate);
     this.setState({visibleModal: null});
@@ -47,7 +56,7 @@ export default class App extends Component {
     if (selectDate !== undefined) {
       this.setState({selectedDate: selectDate, visibleModal: 2})
     }else{
-      alert('No Event Created')
+      this.setState({visibleModal: 3})
     }
   }
 
@@ -59,9 +68,23 @@ export default class App extends Component {
   
   renderModalView = () => (
       <View style={styles.modalContent}>
-        <Text>{this.state.selectedDate.date}</Text>
+        <View style={{flexDirection: 'row'}}>
+          <Text>Event Name : </Text>
+          <Text style={{flex: 1, flexWrap: 'wrap'}}>{this.state.selectedDate.eventName}</Text>
+        </View>
+        <View style={{flexDirection: 'row'}}>
+          <Text>Description : </Text>
+          <Text style={{flex: 1, flexWrap: 'wrap'}}>{this.state.selectedDate.eventDesc}</Text>
+        </View>
         {this.renderButton("Close", () => this.setState({visibleModal: null}), styles.backButton)}
       </View>
+  )
+
+  renderModalNoEvent = () => (
+    <View style={styles.modalContent}>
+      <Text style={styles.noEventText}>No Event Created</Text>
+      {this.renderButton("Close", () => this.setState({visibleModal: null}), styles.backButton)}
+    </View>
   )
 
   renderModalCreate = () => (
@@ -71,7 +94,7 @@ export default class App extends Component {
           <Input placeholderTextColor="black" placeholder="Event Name" onChangeText={(text) => this.setState({eventName: text})} />
         </Item>
         <Item >
-          <Input placeholderTextColor="black" placeholder="Event Description" onChangeText={(text) => this.setState({eventDesc: text})}/>
+          <Input placeholderTextColor="black" placeholder="Description" onChangeText={(text) => this.setState({eventDesc: text})}/>
         </Item>
       </Form>
       <View style={styles.selectDate}>
@@ -99,15 +122,21 @@ export default class App extends Component {
         <Modal isVisible={this.state.visibleModal === 2}>
           {this.renderModalView()}        
         </Modal>
+        <Modal isVisible={this.state.visibleModal === 3}>
+          {this.renderModalNoEvent()}        
+        </Modal>
         <Calendar
           markedDates={this._markedDates()}
           onDayPress={(day) => this.modalView(day)}
+          markingType={'custom'}
         />
         <ActionButton
           buttonColor="#209fd5"
           position="right"
-          renderIcon={active => <Icon style={{color: 'white', fontSize: 26}} name='md-add'/>}
+          renderIcon={active => <Icon style={{color: 'white', fontSize: 24}} name='md-add'/>}
           onPress = {() => this.setState({visibleModal: 1})}
+          offsetX={15}
+          offsetY={15}
         />
       </Container>
     )
@@ -132,5 +161,8 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     marginLeft: 15,
+  },
+  noEventText: {
+    alignSelf: 'center'
   }
 })
